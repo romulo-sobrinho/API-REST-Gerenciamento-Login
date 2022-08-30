afd6aff2d5a04241ebeabed8cc8d180a37ccc619
Romulo <romulosilva001@gmail.com>
2022-08-30 10:34:52 -0300

    Banco mongodb atlas conectado

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const userRouter = require('./routes/userRouter')
+const userRouter = require('./routes/userRouter');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_CONNECTION_URL).then(db => {
+  console.log('Banco de dados conectado')
+}).catch(error => {
+  console.log('Ocorreu um erro ao conectar ao banco de dados')
+})
 
 app.use('/user', userRouter);
 
